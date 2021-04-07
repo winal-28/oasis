1e021ccf6a3920f71e395f20a9e781a08c35889b
Alwin Villero <winalrollevi@gmail.com>
2021-04-07 12:15:14 +0800

    Created the input box

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -3,7 +3,10 @@ import React from 'react'
 export const Home = () => {
     return (
         <main>
-             <img src="./oasislogo.png" alt="oasis logo" className="logo"/>
+            <img src="./oasislogo.png" alt="oasis logo" className="logo" />
+            <form action="submit">
+                <input type="text" placeholder="Search Projects..." className="search" />
+            </form>
         </main>
     )
 }
